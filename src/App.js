8b48bc6689a465b8dcb10fe0812e0fb5d3870966
Charudatta Potdar <charupot08@gmail.com>
2021-07-31 19:24:18 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,60 +1,39 @@
 import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
 import { Route } from "react-router-dom";
-// import dotenv from 'dotenv'
-// import env from 'react-dotenv'
+
 
 import Navbar from "./components/Navbar";
 import PageOne from "./components/PageOne";
 import GSRegLog from "./components/GSRegLog";
 import AdminRegLog from "./components/AdminRegLog";
-import Form from "./components/Form";
 import Error from "./components/Error";
 
-// import './assets/css/boxed.css'
-// import './assets/css/color-styles.css'
-// import './assets/css/dark-boxed.css'
-// import './assets/css/dark-style.css'
-// import './assets/css/side-menu.css'
-// import './assets/css/sidemenu-2.css'
-// import './assets/css/sidemenu-closed.css'
-// import './assets/css/sidemenu-overlay.css'
-// import './assets/css/sidemenu-responsive-tabs.css'
-// import './assets/css/sidemenu.css'
-// import './assets/css/skin-modes.css'
-
-import './css/styled.css'
-// import './assets/css/style.css'
-
-// env.config({path: '../config.env'})
-
+import "./css/styled.css";
 
 function App() {
   return (
     <>
       navbar
       <Navbar />
-
       {/* First page route */}
       <Route exact path="/">
         <PageOne />
       </Route>
-
       {/* Admin Registeration path */}
-      <Route path = "/admin">
-        <AdminRegLog/>
+      <Route path="/admin">
+        <AdminRegLog />
+      </Route>
+      {/* Gram Sevak Registeration Path */}
+      <Route path="/gramsevak">
+        <GSRegLog />
       </Route>
 
-      {/* Gram Sevak Registeration Path */}
-      <Route path = "/gramsevak">
-        <GSRegLog/>
-      </Route>
 
       {/* Error Page */}
       {/* <Route path="">
         <Error/>
       </Route> */}
-
     </>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,8 @@ import { NavLink, useHistory } from "react-router-dom";
 import axios from "axios";
 
 const Form = (props) => {
-
-    const history = useHistory()
+  const history = useHistory();
+  const [flag, setFlag] = useState("");
 
   const [register, setRegister] = useState({
     register_name: "",
@@ -19,7 +19,6 @@ const Form = (props) => {
     login_password: "",
   });
 
-  
   let name, value;
 
   //handling registeration form inputs
@@ -43,167 +42,189 @@ const Form = (props) => {
     event.preventDefault();
 
     let formData = new FormData();
+    if (
+      !register.register_name ||
+      !register.register_email ||
+      !register.register_password ||
+      !register.register_phone ||
+      !register.register_address
+    ) {
+      setFlag("Please fill all the fields");
+    } else {
+      setFlag("");
+      formData.append("name", register.register_name);
+      formData.append("email", register.register_email);
+      formData.append("password", register.register_password);
+      formData.append("phone", register.register_phone);
+      formData.append("address", register.register_address);
 
-    formData.append("name", register.register_name);
-    formData.append("email", register.register_email);
-    formData.append("password", register.register_password);
-    formData.append("phone", register.register_phone);
-    formData.append("address", register.register_address);
+      const url = props.props.url_reg;
 
-    const url = props.props.url_reg;
-
-    await axios
-      .post(url, formData)
-      .then((res) => {
-        if (res.data === 0) {
-          window.alert(`User Registered Successfully!`);
-        //   history.push(props.props.url_page)
-        }
-      })
-      .catch((err) => {
-        window.alert(`error`);
-      });
+      await axios
+        .post(url, formData)
+        .then((res) => {
+          if (res.data === 0) {
+            window.alert(`User Registered Successfully!`);
+            //   history.push(props.props.url_page)
+          }
+        })
+        .catch((err) => {
+          window.alert(`error`);
+        });
+    }
   };
 
   //Logging in user
   const handleLogin = async (event) => {
-      event.preventDefault()
+    event.preventDefault();
 
-      let formData = new FormData()
+    let formData = new FormData();
 
-      formData.append("email", login.login_email)
-      formData.append("password", login.login_password)
+    if (!login.login_email || !login.login_password) {
+      setFlag("Please fill all the fields");
+    } else {
+      setFlag("");
+      formData.append("email", login.login_email);
+      formData.append("password", login.login_password);
 
       const url = props.props.url_log;
 
-      await axios.post(url, formData).then(res => {
-          if(res.data === 123){
-              window.alert(`User logged in successfully!`)
-              history.push("/")
-          }else{
-              window.alert(`error`)
+      await axios
+        .post(url, formData)
+        .then((res) => {
+          if (res.data === 123) {
+            window.alert(`User logged in successfully!`);
+            history.push("/");
+          } else {
+            window.alert(`error`);
           }
-      }).catch(err => console.log(err))
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   //Rendering jsx
   return (
-    <div className="container">
-      <input type="checkbox" id="flip" />
-      <div className="cover"></div>
-      <form action="#">
-        <div className="form-content">
-          {/* Login form */}
-          <div className="login-form">
-            <div className="title">Login</div>
-            <div className="input-boxes">
-              <div className="input-box">
-                <i className="fas fa-envelop"></i>
-                <input
-                  type="email"
-                  placeholder="Enter your email id"
-                  required
-                  name="login_email"
-                  value={login.login_email}
-                  onChange={handleInputLogin}
-                />
-              </div>
-              <div className="input-box">
-                <i className="fas fa-envelop"></i>
-                <input
-                  type="password"
-                  placeholder="Enter your password"
-                  required
-                  name="login_password"
-                  value={login.login_password}
-                  onChange={handleInputLogin}
-                />
-              </div>
-              <div className="button input-box">
-                <i className="fas fa-envelop"></i>
-                <button type="submit" onClick={handleLogin}>
-                  Submit
-                </button>
-              </div>
-              <div className="text">
-                Don't have an account ? <label for="flip">Sign Up now</label>{" "}
+    <>
+      <div className="container">
+        <input type="checkbox" id="flip" />
+        <div className="cover"></div>
+        <form action="#">
+          <div className="form-content">
+            {/* Login form */}
+            <div className="login-form">
+              <div className="title">Login</div>
+              <h5>{flag}</h5>
+              <div className="input-boxes">
+                <div className="input-box">
+                  <i className="fas fa-envelop"></i>
+                  <input
+                    type="email"
+                    placeholder="Enter your email id"
+                    required
+                    name="login_email"
+                    value={login.login_email}
+                    onChange={handleInputLogin}
+                  />
+                </div>
+                <div className="input-box">
+                  <i className="fas fa-envelop"></i>
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    required
+                    name="login_password"
+                    value={login.login_password}
+                    onChange={handleInputLogin}
+                  />
+                </div>
+                <div className="button input-box">
+                  <i className="fas fa-envelop"></i>
+                  <button className="" type="submit" onClick={handleLogin}>
+                    Submit
+                  </button>
+                </div>
+                <div className="text">
+                  Don't have an account ? <label for="flip">Sign Up now</label>{" "}
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* Registeration form */}
-          <div className="Signup-form">
-            <div className="title">Register</div>
-            <div className="input-boxes">
-              <div className="input-box">
-                <i className="fas fa-envelop"></i>
-                <input
-                  type="text"
-                  placeholder="Enter your Username"
-                  required
-                  name="register_name"
-                  value={register.register_name}
-                  onChange={handleInputRegister}
-                />
-              </div>
-              <div className="input-box">
-                <i className="fas fa-envelop"></i>
-                <input
-                  type="email"
-                  placeholder="Enter your email id"
-                  required
-                  name="register_email"
-                  value={register.register_email}
-                  onChange={handleInputRegister}
-                />
-              </div>
-              <div className="input-box">
-                <i className="fas fa-envelop"></i>
-                <input
-                  type="password"
-                  placeholder="Enter your password"
-                  required
-                  name="register_password"
-                  value={register.register_password}
-                  onChange={handleInputRegister}
-                />
-              </div>
-              <div className="input-box">
-                <i className="fas fa-envelop"></i>
-                <input
-                  type="text"
-                  placeholder="Enter your Address"
-                  required
-                  name="register_address"
-                  value={register.register_address}
-                  onChange={handleInputRegister}
-                />
-              </div>
-              <div className="input-box">
-                <i className="fas fa-envelop"></i>
-                <input
-                  type="text"
-                  placeholder="Enter your Phone No."
-                  required
-                  name="register_phone"
-                  value={register.register_phone}
-                  onChange={handleInputRegister}
-                />
-              </div>
-              <div className="button input-box">
-                <i className="fas fa-envelop"></i>
-                <button type="submit" onClick={handleRegister}>
-                  Submit
-                </button>
-              </div>
-              <div className="text">
-                Already have an account? <label for="flip">Login now</label>
+            {/* Registeration form */}
+            <div className="Signup-form">
+              <div className="title">Register</div>
+              <h5>{flag}</h5>
+              <div className="input-boxes">
+                <div className="input-box">
+                  <i className="fas fa-envelop"></i>
+                  <input
+                    type="text"
+                    placeholder="Enter your Username"
+                    required
+                    name="register_name"
+                    value={register.register_name}
+                    onChange={handleInputRegister}
+                  />
+                </div>
+                <div className="input-box">
+                  <i className="fas fa-envelop"></i>
+                  <input
+                    type="email"
+                    placeholder="Enter your email id"
+                    required
+                    name="register_email"
+                    value={register.register_email}
+                    onChange={handleInputRegister}
+                  />
+                </div>
+                <div className="input-box">
+                  <i className="fas fa-envelop"></i>
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    required
+                    name="register_password"
+                    value={register.register_password}
+                    onChange={handleInputRegister}
+                  />
+                </div>
+                <div className="input-box">
+                  <i className="fas fa-envelop"></i>
+                  <input
+                    type="text"
+                    placeholder="Enter your Address"
+                    required
+                    name="register_address"
+                    value={register.register_address}
+                    onChange={handleInputRegister}
+                  />
+                </div>
+                <div className="input-box">
+                  <i className="fas fa-envelop"></i>
+                  <input
+                    type="text"
+                    placeholder="Enter your Phone No."
+                    required
+                    name="register_phone"
+                    value={register.register_phone}
+                    onChange={handleInputRegister}
+                  />
+                </div>
+                <div className="button input-box">
+                  <i className="fas fa-envelop"></i>
+                  <button type="submit" onClick={handleRegister}>
+                    Submit
+                  </button>
+                </div>
+                <div className="text">
+                  Already have an account? <label for="flip">Login now</label>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 };
 
