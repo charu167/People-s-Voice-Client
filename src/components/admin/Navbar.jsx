58b2af1755d0368f587028a47aa64c8331e0cd09
Charudatta Potdar <charupot08@gmail.com>
2021-08-13 14:18:41 +0530

    minor changes

--- src/components/admin/Navbar.jsx
+++ src/components/admin/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light mt-0 border-bottom-5 fixed-top Navbar">
+      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light mt-0 border-bottom-5 fixed-top Navbar">
       <div className="d-flex justify-content-around">
         <NavLink className="navbar-brand" to="#">
           Logo
@@ -32,54 +32,49 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-    </nav>
+    </nav> */}
 
-      {/* <header class="app-header header">
-        <div class="container-fluid">
-          <div class="d-flex">
-            <a class="header-brand" href="index.html">
-              
-            </a>
+      <header className="app-header header">
+        <div className="container-fluid">
+          <div className="d-flex">
+            <NavLink className="header-brand" to="/">
+              LOGO
+            </NavLink>
 
-            <a
+            <NavLink
               aria-label="Hide Sidebar"
-              class="app-sidebar__toggle"
+              className="app-sidebar__toggle"
               data-toggle="sidebar"
-              href="#"
-            ></a>
-            <div class="dropdown d-sm-flex d-none">
-              <div class="dropdown-menu header-search dropdown-menu-left dropdown-menu-arrow">
-                <div class="input-group w-100 p-2">
-                  <div class="input-group-append ">
-                    <button type="button" class="btn btn-primary ">
-                      <i class="fas fa-search" aria-hidden="true"></i>
-                    </button>
-                  </div>
-                </div>
+              to="#"
+            ></NavLink>
+
+            <div className="d-flex order-lg-2 ml-auto">
+              <div className="dropdown d-sm-flex d-none header-message">
+                <NavLink
+                  className="nav-link icon"
+                  data-toggle="dropdown"
+                  to={`${path}/priority`}
+                >
+                  <i className="fe fe-bell"></i>
+                  <span className=" nav-unread badge badge-warning  badge-pill">
+                    
+                  </span>
+                </NavLink>
               </div>
-            </div>
 
-            <div class="d-flex order-lg-2 ml-auto">
-              
-              <button
-                class="navbar-toggler navresponsive-toggler d-sm-none"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent-4"
-                aria-controls="navbarSupportedContent-4"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span class="navbar-toggler-icon fe fe-more-vertical text-white"></span>
-              </button>
-
-              
-                
-              
+              <div className="dropdown">
+                <NavLink
+                  className="nav-link pr-0 leading-none d-flex"
+                  data-toggle="dropdown"
+                  to={`${path}/settings`}
+                >
+                  <img src="https://img.icons8.com/color/50/000000/settings.png" />
+                </NavLink>
+              </div>
             </div>
           </div>
         </div>
-      </header> */}
+      </header>
     </>
   );
 };
